f44e41c7df1cec9b8d2e3d0d4df3b5a499066ff6
DESKTOP-QUGCJKS\STL04 <junios@junios.net>
2017-05-20 18:45:46 +0900

    FSM 추가

--- Assets/Scripts/RayMove.cs
+++ Assets/Scripts/RayMove.cs
@@ -11,10 +11,14 @@ public class RayMove : MonoBehaviour
     public float moveSpeed = 4.0f;
     public float turnSpeed = 360.0f;
 
-    public bool isRun = false;
+    public CharacterState state = CharacterState.Idle;
 
+    Animator a;
    
     NavMeshAgent agent;
+
+    public LayerMask layerMask;
+    public float attackRange = 1.5f;
 
     void Awake()
     {
@@ -24,29 +28,75 @@ public class RayMove : MonoBehaviour
         agent.speed = moveSpeed;
         agent.angularSpeed = turnSpeed;
         agent.acceleration = 2000.0f;
+
+        a = GetComponent<Animator>();
+
+        layerMask = LayerMask.GetMask("Click", "Block", "Monster");
     }
 
     void Update()
     {
-        if (agent.remainingDistance == 0)
+        switch (state)
         {
-            isRun = false;
-            movePoint.SetActive(false);
+            case CharacterState.Idle:
+                break;
+
+            case CharacterState.Run:
+                if (agent.remainingDistance == 0)
+                {
+                    state = CharacterState.Idle;
+                    movePoint.SetActive(false);
+                }                
+                break;
+
+            case CharacterState.AttackRun:
+                if (agent.remainingDistance <= attackRange)
+                {
+                    state = CharacterState.Attack;
+                }
+                else
+                {
+                    agent.SetDestination(attackPoint.transform.position);
+                }
+                break;
+
+            case CharacterState.Attack:
+                break;
         }
+
 
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
 
-            if (Physics.Raycast(ray, out hitInfo, 100.0f))
+            if (Physics.Raycast(ray, out hitInfo, 100.0f, layerMask))
             {
-                Debug.Log(hitInfo.point);
-                movePoint.transform.position = hitInfo.point;
-                movePoint.SetActive(true);
-                agent.SetDestination(movePoint.transform.position);
-                isRun = true;
+                int layer = hitInfo.collider.gameObject.layer;
+
+                if (layer == LayerMask.NameToLayer("Click"))
+                {
+                    movePoint.transform.position = hitInfo.point;
+                    movePoint.SetActive(true);
+                    attackPoint.SetActive(false);
+
+                    agent.SetDestination(movePoint.transform.position);
+                    state = CharacterState.Run;
+                    agent.stoppingDistance = 0;
+                }
+                else if (layer == LayerMask.NameToLayer("Monster"))
+                {
+                    attackPoint.transform.SetParent(hitInfo.transform);
+                    attackPoint.transform.localPosition = Vector3.zero;
+                    attackPoint.SetActive(true);
+                    movePoint.SetActive(false);
+                    agent.SetDestination(attackPoint.transform.position);
+                    state = CharacterState.AttackRun;
+                    agent.stoppingDistance = attackRange;
+                }
             }
         }
+
+        a.SetInteger("state", (int)state);
     }
 }
